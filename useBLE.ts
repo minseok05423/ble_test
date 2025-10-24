55ec6abbf1e65554ca5c3721c5a090e6944be07a
minseok05423 <106617123+minseok05423@users.noreply.github.com>
2025-10-24 13:11:18 +0900

    just keeping everything up to date

--- useBLE.ts
+++ useBLE.ts
@@ -1,4 +1,11 @@
+import { useMemo } from "react";
+import { BleManager } from "react-native-ble-plx";
+
 interface BluetoothLowEnergyApi {
   requestPermissions(): Promise<boolean>;
   scanForPeripherals(): void;
+}
+
+function useBLE(): BluetoothLowEnergyApi {
+  const bleManager = useMemo(() => new BleManager(), []);
 }
