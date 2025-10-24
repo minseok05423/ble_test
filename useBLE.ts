955538690ed311a220dafaf890427860f27cfd16
Minseok <minseok05423@gmail.com>
2025-10-24 19:09:05 +0900

    added useBle hook

--- useBLE.ts
+++ useBLE.ts
@@ -1,5 +1,7 @@
-import { useMemo } from "react";
-import { BleManager } from "react-native-ble-plx";
+import * as ExpoDevice from "expo-device";
+import { useMemo, useState } from "react";
+import { PermissionsAndroid, Platform } from "react-native";
+import { BleManager, Device } from "react-native-ble-plx";
 
 interface BluetoothLowEnergyApi {
   requestPermissions(): Promise<boolean>;
@@ -8,4 +10,68 @@ interface BluetoothLowEnergyApi {
 
 function useBLE(): BluetoothLowEnergyApi {
   const bleManager = useMemo(() => new BleManager(), []);
+
+  const [allDevices, setAllDevices] = useState<Device[]>([]);
+
+  const requestAndroid31Permissions = async () => {
+    const bluetoothScanPermissions = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
+      {
+        title: "Scan Permission",
+        message: "App requires Bluetooth Scanning",
+        buttonPositive: "OK",
+      }
+    );
+    const bluetoothConnectPermissions = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+      {
+        title: "Connect Permission",
+        message: "App requires Bluetooth connection",
+        buttonPositive: "OK",
+      }
+    );
+    const bluetoothFineLocationPermissions = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: "Fine Location Permission",
+        message: "App requires fine location",
+        buttonPositive: "OK",
+      }
+    );
+
+    return (
+      bluetoothScanPermissions === "granted" &&
+      bluetoothConnectPermissions === "granted" &&
+      bluetoothFineLocationPermissions === "granted"
+    );
+  };
+
+  const requestPermissions = async () => {
+    if (Platform.OS === "android") {
+      if ((ExpoDevice.platformApiLevel ?? -1) < 31) {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+          {
+            title: "Location Permission",
+            message: "Bluetooth requires Location",
+            buttonPositive: "OK",
+          }
+        );
+
+        return granted === PermissionsAndroid.RESULTS.GRANTED;
+      } else {
+        const isAndroid31PermissionsGranted =
+          await requestAndroid31Permissions();
+        return isAndroid31PermissionsGranted;
+      }
+    } else {
+      return true;
+    }
+  };
+
+  const scanForPeripherals = () => {};
+
+  return { scanForPeripherals, requestPermissions };
 }
+
+export default useBLE;
